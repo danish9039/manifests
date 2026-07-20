kubeflow/community-distribution

helm: move oauth2-proxy chart to common/oauth2-proxy/helm
Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

tests/helm_kustomize_compare.sh
@@ -133,7 +133,7 @@ case "$COMPONENT" in
         ;;
 
     "oauth2-proxy")
-        CHART_DIR="$ROOT_DIR/experimental/helm/charts/oauth2-proxy"
+        CHART_DIR="$ROOT_DIR/common/oauth2-proxy/helm"
         MANIFESTS_DIR="$ROOT_DIR/common/oauth2-proxy"
 
         declare -A KUSTOMIZE_PATHS=(

danish9039/manifests

tests/helm_kustomize_compare.sh
@@ -215,7 +215,7 @@ case "$COMPONENT" in
         ;;
 
     "oauth2-proxy")
-        CHART_DIRECTORY="$ROOT_DIRECTORY/experimental/helm/charts/oauth2-proxy"
+        CHART_DIRECTORY="$ROOT_DIRECTORY/common/oauth2-proxy/helm"
         MANIFESTS_DIRECTORY="$ROOT_DIRECTORY/common/oauth2-proxy"
 
         declare -A KUSTOMIZE_PATHS=(