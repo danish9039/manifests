kubeflow/community-distribution

helm: move dex chart to common/dex/helm
Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

tests/helm_kustomize_compare.sh
@@ -133,7 +133,7 @@ case "$COMPONENT" in
         ;;
 
     "dex")
-        CHART_DIR="$ROOT_DIR/experimental/helm/charts/dex"
+        CHART_DIR="$ROOT_DIR/common/dex/helm"
         MANIFESTS_DIR="$ROOT_DIR/common/dex"
 
         declare -A KUSTOMIZE_PATHS=(

danish9039/manifests

tests/helm_kustomize_compare.sh
@@ -206,7 +206,7 @@ case "$COMPONENT" in
         ;;
 
     "dex")
-        CHART_DIRECTORY="$ROOT_DIRECTORY/experimental/helm/charts/dex"
+        CHART_DIRECTORY="$ROOT_DIRECTORY/common/dex/helm"
         MANIFESTS_DIRECTORY="$ROOT_DIRECTORY/common/dex"
 
         declare -A KUSTOMIZE_PATHS=(